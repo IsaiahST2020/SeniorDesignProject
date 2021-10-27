b19dbf6f822beee48e62a7491979ae3a20ebea17
Kali <kali@kali>
2021-10-27 17:08:25 -0400

    Renamed trydjango to PrinterSite

--- PrinterSite/urls.py
+++ PrinterSite/urls.py
@@ -1,4 +1,4 @@
-"""trydjango URL Configuration
+"""PrinterSite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
