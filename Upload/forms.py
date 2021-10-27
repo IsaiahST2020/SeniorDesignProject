1b3094f2ddf028eb2a01463c7a6d32ad14ac87ea
Rad10 <nrc1mr.e3@gmail.com>
2021-10-27 18:28:15 -0400

    Converted all these files to use spaces instead of tabs

--- Upload/forms.py
+++ Upload/forms.py
@@ -2,29 +2,31 @@ from django import forms
 
 from .models import Upload, FileUpload
 
+
 class UploadForm(forms.ModelForm):
-	class Meta:
-		model = Upload
-		fields = [
-			'title',
-			'quantity'
-		]
-	def clean_title(self, *args, **kwargs):
-		title = self.cleaned_data.get("title")
-		if "CFE" in title:
-			return clean_title
-		else:
-			raise forms.ValidationError("this is not a valid title")
+    class Meta:
+        model = Upload
+        fields = [
+            'title',
+            'quantity'
+        ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "CFE" in title:
+            return clean_title
+        else:
+            raise forms.ValidationError("this is not a valid title")
 
 
 class RawUploadForm(forms.Form):
-	title = forms.CharField(required=True)
-	number = forms.CharField(label='')
-	date = forms.CharField()
-	quantity = forms.IntegerField(initial=1)
+    title = forms.CharField(required=True)
+    number = forms.CharField(label='')
+    date = forms.CharField()
+    quantity = forms.IntegerField(initial=1)
 
 
 class FileForm(forms.ModelForm):
-	class Meta:
-		model = FileUpload
-		fields = ('title', 'file', )
+    class Meta:
+        model = FileUpload
+        fields = ('title', 'file', )
