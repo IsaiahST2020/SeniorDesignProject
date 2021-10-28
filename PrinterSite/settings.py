b154a9d055402b4c53e036bef8906b80fd9af752
Kali <kali@kali>
2021-10-28 19:14:08 -0400

    Linked the basic style sheet and all the pages share the same style

--- PrinterSite/settings.py
+++ PrinterSite/settings.py
@@ -120,9 +120,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+#STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
